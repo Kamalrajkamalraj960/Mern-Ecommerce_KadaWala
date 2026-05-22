b0c8c00aa7db5e90b72f3ace0b7ec6440b080b1b
= <kamalrajmanu@gmail.com>
2026-05-22 20:28:32 +0530

    deployed

--- backend/server.js
+++ backend/server.js
@@ -20,8 +20,8 @@ const app = express();
 
 // Standard middlewares
 app.use(cors({
-  origin: "https://mern-ecommerce-kadawala.vercel.app/",
-  credentials: true
+  origin: 'https://mern-ecommerce-kadawala.vercel.app',
+  credentials: true,
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
